47077025b67bcd3b3cc0274c7e149d0c7f71c72f
pier-ex <nathanaelrog@hotmail.com>
2026-05-13 13:04:19 -0300

    feat: adiciona exercícios de CRUD e controle de estoque

--- pratica_arrays3/main.js
+++ pratica_arrays3/main.js
@@ -154,45 +154,176 @@
 // console.log(usuarios)
 // console.log(contador)
 
-const tarefas = [
-    {nome: 'estudar js', concluida: false},
-    {nome : 'treinar', concluida: true},
-    {nome: 'ler um livro', concluida: false}
+// const tarefas = [
+//     {nome: 'estudar js', concluida: false},
+//     {nome : 'treinar', concluida: true},
+//     {nome: 'ler um livro', concluida: false}
+// ]
+
+// console.log(tarefas);
+
+// for(let dados of tarefas) {
+//     console.log(dados.nome)
+//     console.log(dados.concluida)
+// }
+
+// for(let dados of tarefas) {
+//     if(dados.concluida) {
+//         console.log(dados)
+//     }
+// }
+
+// let contador = 0
+
+// for(let dados of tarefas) {
+//     if(dados.concluida === false) {
+//         contador++
+//     }
+// }
+
+// console.log(contador)
+
+// tarefas.push({nome: 'Estudar arrays', concluida: false})
+
+// console.log(tarefas)
+
+// for(let dados of tarefas) {
+//     if(dados.nome === 'ler um livro') {
+//         dados.concluida = true
+//     }
+// }
+
+// tarefas.splice(1,1)
+
+// const estoque = [
+//     {produtos: 'mouse', quantidade: 10},
+//     {produtos: 'teclado', quantidade: 5},
+//     {produtos: 'monitor', quantidade: 2},
+// ]
+
+// console.log(estoque);
+
+// for(let dados of estoque) {
+//     if(dados.quantidade < 6) {
+//         console.log(dados)
+//     }
+// }
+
+// estoque.push({produtos: 'headset', quantidade: 3})
+
+// for(let dados of estoque) {
+//     if(dados.produtos === 'monitor') {
+//         dados.quantidade = 1
+//     }
+// }
+
+// estoque.splice(2,1)
+
+// let contador = 0
+
+// for(let dados of estoque) {
+//     if(dados.quantidade < 3) {
+//         contador++
+//     }
+// }
+
+// console.log(contador)
+
+// const filmes = [
+//     {nome: 'batman', categoria: 'ação', assistido: true},
+//     {nome: 'shrek', categoria: 'animação', assistido: false},
+//     {nome: 'interestelar', categoria: 'ficção', assistido: true},
+//     {nome: 'vingadores', categoria: 'ação', assistido: false}
+// ]
+
+// console.log(filmes)
+
+// for(let dados of filmes) {
+//     console.log(dados.nome)
+//     console.log(dados.categoria)
+//     console.log(dados.assistido)
+// }
+
+// for(let dados of filmes) {
+//     if(dados.assistido) {
+//         console.log(dados)
+//     }
+// }
+
+// filmes.push({nome: 'toy story', categoria: 'animação', assistido: true})
+
+// for(let dados of filmes) {
+//     if(dados.nome === 'shrek') {
+//         dados.assistido = true
+//     }
+// }
+
+
+// filmes.splice(3,1)
+
+// let contador = 0
+
+// for(let dados of filmes) {
+//     if(dados.assistido) {
+//         contador++
+//     }
+// }
+
+// console.log(contador)
+
+
+const produtos = [
+    {nome: 'mouse', preco: 120, estoque: 10},
+    {nome: 'teclado', preco: 250, estoque: 5},
+    {nome: 'monitor', preco: 900, estoque: 2},
 ]
 
-console.log(tarefas);
+console.log(produtos)
 
-for(let dados of tarefas) {
+for(let dados of produtos) {
     console.log(dados.nome)
-    console.log(dados.concluida)
+    console.log(dados.preco)
+    console.log(dados.estoque)
 }
 
-for(let dados of tarefas) {
-    if(dados.concluida) {
+for(let dados of produtos) {
+    if(dados.estoque < 6) {
         console.log(dados)
     }
 }
 
+for(let dados of produtos) {
+    if(dados.preco < 200) {
+        console.log(dados)
+    }
+}
+
+produtos.push({nome: 'headset', preco: 180, estoque: 8})
+
+console.log(produtos)
+
+for(let dados of produtos) {
+    if(dados.nome === 'monitor') {
+        dados.estoque = 7
+    }
+}
+
+produtos.splice(1,1)
+
 let contador = 0
 
-for(let dados of tarefas) {
-    if(dados.concluida === false) {
+for(let dados of produtos) {
+    if(dados.estoque > 5) {
         contador++
     }
 }
 
 console.log(contador)
 
-tarefas.push({nome: 'Estudar arrays', concluida: false})
+let soma = 0
 
-console.log(tarefas)
-
-for(let dados of tarefas) {
-    if(dados.nome === 'ler um livro') {
-        dados.concluida = true
-    }
+for(let dados of produtos) {
+    soma += dados.preco * dados.estoque
 }
 
-tarefas.splice(1,1)
-
-
+console.log(soma)
