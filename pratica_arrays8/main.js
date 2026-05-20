1a12e3a9f9c766af85de9df0ab76fceaab3b63c6
pier-ex <nathanaelrog@hotmail.com>
2026-05-20 10:29:33 -0300

    study: algoritmos de maior e menor valor em arrays

--- pratica_arrays8/main.js
+++ pratica_arrays8/main.js
@@ -1,87 +1,226 @@
-const notas = [7 ,10 ,5 ,8 ,3 ,9 ,6 ,1 ]
+// const notas = [7 ,10 ,5 ,8 ,3 ,9 ,6 ,1 ]
 
-console.log(notas)
+// console.log(notas)
 
-console.log(notas.sort((a,b) => a -b))
+// console.log(notas.sort((a,b) => a -b))
 
-console.log(notas.sort((a,b) => b -a))
+// console.log(notas.sort((a,b) => b -a))
 
-for(let i = 0; i < notas.length; i ++) {
-    if(notas[i] > 7) {
-        console.log(notas[i])
+// for(let i = 0; i < notas.length; i ++) {
+//     if(notas[i] > 7) {
+//         console.log(notas[i])
+//     }
+
+//     if(notas[i] < 6) { 
+//         console.log(notas[i])
+//     }
+// }
+
+
+// for(let i = 0; i < notas.length; i++) {
+//     if(notas[i] % 2 === 0) {
+//         console.log(notas[i])
+//     }
+
+//     if(notas[i] % 2 === 1) {
+//         console.log(notas[i])
+//     }
+// }
+
+// let maiorNota = notas[0]
+// let menorNota = notas[0]
+
+// for(let i = 0; i < notas.length; i ++) {
+//     if(notas[i] > maiorNota) {
+//         maiorNota = notas[i]
+//     }
+// }
+
+// console.log(maiorNota)
+
+// for(let i = 0; i < notas.length; i++) {
+//     if(notas[i] < menorNota) {
+//         menorNota = notas[i]
+//     }
+// }
+
+// console.log(menorNota)
+
+// let contador = 0 
+
+// for(let i = 0; i < notas.length; i++) {
+//     contador += notas[i]
+// }
+
+// console.log(contador)
+
+// let media = 0
+
+// for(let i = 0; i < notas.length; i++) {
+//     media = contador / notas.length
+// }
+
+// console.log(media)
+
+// let maiorQue7 = 0
+
+// for(let i = 0; i < notas.length; i++) {
+//     if(notas[i] >= 7) {
+//         maiorQue7 = notas[i]
+//         console.log(maiorQue7)
+//     }
+// }
+// console.log(notas.join(' | '))
+
+
+// notas.push(4)
+
+// notas.push(11)
+
+// console.log(notas)
+
+// notas.sort((a,b)=> a -b)
+
+// console.log(notas)
+
+
+const vendas = [120,80,300,50,25,500,210,95]
+
+console.log(vendas)
+
+
+vendas.sort((a,b) => a -b)
+
+console.log(vendas)
+
+vendas.sort((a,b) => b  -a)
+
+console.log(vendas)
+
+for(let i = 0; i < vendas.length; i ++) {
+    if(vendas[i] > 100) {
+        console.log(vendas[i])
     }
 
-    if(notas[i] < 6) { 
-        console.log(notas[i])
+    if(vendas[i] < 90) {
+        console.log(vendas[i])
+    }
+
+
+}
+
+for(let i = 0; i < vendas.length; i ++) {
+    if(vendas[i] % 2 === 0) {
+        console.log(vendas[i])
+    }
+}
+
+for(let i = 0; i < vendas.length; i ++) {
+
+    if(vendas[i] % 2 === 1) {
+        console.log(vendas[i])
+
     }
 }
 
 
-for(let i = 0; i < notas.length; i++) {
-    if(notas[i] % 2 === 0) {
-        console.log(notas[i])
-    }
+let maiorVenda = vendas[0]
 
-    if(notas[i] % 2 === 1) {
-        console.log(notas[i])
+for(let i = 0; i < vendas.length; i++) {
+    if(vendas[0] > maiorVenda ) {
+        maiorVenda = vendas[i]
     }
 }
 
-let maiorNota = notas[0]
-let menorNota = notas[0]
+console.log(maiorVenda)
 
-for(let i = 0; i < notas.length; i ++) {
-    if(notas[i] > maiorNota) {
-        maiorNota = notas[i]
+let menorVenda = vendas[0]
+
+for(let i =0; i < vendas.length; i ++) {
+    if(menorVenda < vendas[0]) {
+        menorVenda = vendas[i]
     }
 }
 
-console.log(maiorNota)
+console.log(menorVenda)
 
-for(let i = 0; i < notas.length; i++) {
-    if(notas[i] < menorNota) {
-        menorNota = notas[i]
-    }
+let somaTotal = 0
+
+for(let i = 0; i < vendas.length; i ++) {
+    somaTotal += vendas[i]
 }
 
-console.log(menorNota)
-
-let contador = 0 
-
-for(let i = 0; i < notas.length; i++) {
-    contador += notas[i]
-}
-
-console.log(contador)
+console.log(somaTotal)
 
 let media = 0
 
-for(let i = 0; i < notas.length; i++) {
-    media = contador / notas.length
+for(let i = 0; i < vendas.length; i ++) {
+    media = somaTotal / 8
 }
 
 console.log(media)
 
-let maiorQue7 = 0
+let vendasMaiorQue100 = 0
 
-for(let i = 0; i < notas.length; i++) {
-    if(notas[i] >= 7) {
-        maiorQue7 = notas[i]
-        console.log(maiorQue7)
+for(let i = 0; i < vendas.length; i ++) {
+    if(vendas[i] >= 100) {
+        vendasMaiorQue100++
     }
 }
-console.log(notas.join(' | '))
+
+console.log(vendasMaiorQue100)
+
+console.log(vendas.join(' | '))
+
+vendas.push(700)
+
+vendas.push(40)
+
+console.log(vendas)
+
+vendas.sort((a,b) => a -b)
+
+console.log(vendas)
+
+maiorVenda = vendas[0]
+menorVenda = vendas[0]
+
+for(let i = 0; i < vendas.length; i ++) {
+    if(vendas[i] > maiorVenda) {
+        maiorVenda = vendas[i]
+    }
 
 
-notas.push(4)
+    if(vendas[i] < menorVenda  ) {
+        menorVenda = vendas[i]
+    }
+}
 
-notas.push(11)
+console.log(maiorVenda)
 
-console.log(notas)
+console.log(menorVenda)
 
-notas.sort((a,b)=> a -b)
+media = 0
 
-console.log(notas)
+for(let i = 0; i < vendas.length; i ++) {
+    media += vendas[i] / 10
+}
+
+console.log(media)
+
+let segundaMenorVenda = vendas[0]
+
+for(let i = 0; i < vendas.length; i ++) {
+    if(segundaMenorVenda > menorVenda) {
+        segundaMenorVenda = vendas[i]
+    }
+}
+
+console.log(segundaMenorVenda)
+
+
+
+
 
 
 
